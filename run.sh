dd97e2cbc54be345ed8359749cbb5005aad8b426
cgpavlakos <cgpavlakos@gmail.com>
2024-05-20 10:38:21 -0700

    updates

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
+# Allow traffic on port 8502 (replace with your desired port if needed)
 sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 8502 -j ACCEPT
+
+# Activate virtual environment and navigate to app directory
 cd ~/src/llama/llamaenv/bin
 source activate
 cd ~/src/GenAI-Demo-main
+
+# Run the application in the background
 nohup streamlit run streamlit_app_oci.py &
+
+echo "Application started in the background."
